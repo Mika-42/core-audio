2c78afd08cc67089a3a2bb5c9ab7d39cc0eb9f64
Matis <matis.mikael@gmail.com>
2026-03-06 12:11:07 +0100

    update

--- src/main.cpp
+++ src/main.cpp
@@ -9,6 +9,8 @@ void audio_callback(mka::audio::Block& block) {
 
 	for (uint32_t i = 0; i < block.midiEventCount; ++i) {
 		const auto& midi = block.midiEvents[i];
+
+		std::println("frame offset: {}, size: {}, [{}, {}, {}]", midi.frameOffset, midi.size, midi.data[0], midi.data[1], midi.data[2]);
 		(void)midi;
 	}
 
@@ -40,17 +42,23 @@ int main() {
 		std::println("direction:\t{}\n", ch.direction == mka::audio::Direction::In ? "Input" : "Output");
     }
 	
+	// [1] select one of them
+	int choice = 0;
+	std::print(">> enter number: ");
+	std::cin >> choice;
+
+	//[]	
 	auto MIDIchannels = engine.getMidiDevices();
     for (auto& ch : MIDIchannels) {	
 		std::println("channel name:\t{}", ch.name);
 		std::println("direction:\t{}\n", ch.direction == mka::audio::Direction::In ? "Input" : "Output");
     }
 
-	// [1] select one of them
-	int choice = 0;
 	std::print(">> enter number: ");
 	std::cin >> choice;
 	
+	engine.mapMidiDevice(MIDIchannels[choice], 0);
+
 	// [2] setup the engine
 	engine.setCallback(audio_callback);
 	engine.setSampleRate(22'050);
